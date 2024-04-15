d7dd3d376d4abba34e4ea700ff9b7709a4e2cfac
David <david_sanchez@berkeley.edu>
2024-04-15 09:59:56 -0700

    front

--- src/VideoList.jsx
+++ src/VideoList.jsx
@@ -1,1 +1,25 @@
 // Scroll area which will contain the list of videos after the search.
+
+// Scroll area which will contain the list of videos after the search.
+
+import React from 'react';
+import VideoDisplay from './VideoDisplay';
+
+
+
+function VideoList() {
+    // Simulating video data
+    const videos = [
+      // Populate with video data objects
+    ];
+  
+    return (
+      <div className="videoList">
+        {videos.map(video => (
+          <VideoDisplay key={video.id} video={video} />
+        ))}
+      </div>
+    );
+  }
+  
+  export default VideoList;
